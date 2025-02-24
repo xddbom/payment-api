e5a9e96d78285533c781c5e6f22ede7f41ca8781
xddbom <xddbom@gmail.com>
2025-02-25 01:05:45 +0200

    Setup swagger for the project

--- delivery/http/routes.go
+++ delivery/http/routes.go
@@ -4,14 +4,31 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func RoutesSetup(r *gin.Engine) {
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hello world!"})
-	})
+// @Summary Home page
+// @Tags common
+// @Success 200 {object} map[string]string
+// @Router / [get]
+func HomeHandler(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "Hello world!"})
+}
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "pong"})
-	})
+// @Summary Server availability check
+// @Tags common
+// @Success 200 {object} map[string]string
+// @Router /ping [get]
+func PingHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "pong"})
+}
+
+func SetupRoutes(r *gin.Engine) {
+	r.GET("/", HomeHandler)
+	r.GET("/ping", PingHandler)
+
+	// Swagger
+	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json")
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 }
--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"payment-api/delivery/http"
+	_ "payment-api/docs"
 )
 
 func main() {
 	r := gin.Default()
 
-	http.RoutesSetup(r)
+	http.SetupRoutes(r)
 
 	r.Run(":8080")
 }
