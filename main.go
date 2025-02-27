360a073f99e9d3dc51f5915bc08d37d18b4468bd
xddbom <xddbom@gmail.com>
2025-02-27 21:54:50 +0200

    Setup HTTPS with HTTP/2 serving with basic HTTP/1.1 on different ports.

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"crypto/tls"
+	"log"
+	"net/http"
 
-	"payment-api/delivery/http"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/net/http2"
+
+	delivery "payment-api/delivery/http"
 	_ "payment-api/docs"
 )
 
 func main() {
 	r := gin.Default()
-	http.SetupRoutes(r)
+	delivery.SetupRoutes(r)
 
-	
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+		TLSConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
+		},
+	}
 
-	r.Run(":8080")
+	go func() {
+		log.Println("HTTP started on port :8081")
+		if err := r.Run(":8081"); err != nil {
+			log.Printf("HTTP stoped. %v", err)
+		}
+	}()
+
+	http2.ConfigureServer(server, &http2.Server{})
+	log.Println("HTTPS with HTTP/v2 started on port :8080")
+	log.Fatal(server.ListenAndServeTLS("./infrastructure/certs/cert.pem", "./infrastructure/certs/key.pem"))
 }
