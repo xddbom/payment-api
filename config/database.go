07b6fd2f208a08e30a5e0e4cdf921e072158b84d
xddbom <xddbom@gmail.com>
2025-03-06 21:01:14 +0200

    Realized simple db config.

---
+++ config/database.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"os"
+)
+
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewDBConfig() DBConfig {
+	return DBConfig{
+		Host:     getEnv("DB_HOST", "localhost"),
+		Port:     getEnv("DB_PORT", "5432"),
+		User:     getEnv("DB_USER", "postgres"),
+		Password: getEnv("DB_PASSWORD", "password"),
+		DBName:   getEnv("DB_NAME", "myapp"),
+		SSLMode:  getEnv("DB_SSLMODE", "disable"),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
